249c967f26eb3ae935eea567b58b827d65bbb50a
DocGroth <bgr@tradeshift.com>
2018-01-26 17:14:46 +0100

    New Version

--- src/main/java/brian/BreakBot.java
+++ src/main/java/brian/BreakBot.java
@@ -1,23 +1,39 @@
 package bgr;
 
 import robocode.*;
+import robocode.util.Utils;
+
+import java.awt.Color;
 
 public class BreakBot extends Robot {
 	
 	static double foundHeading = 0;
 	
 	public void run() {
+		setColors(Color.CYAN,Color.MAGENTA,Color.ORANGE);
+		
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);	
+		
         while (true) {
-            ahead(200);
-            turnLeft(foundHeading);
-            turnGunRight(foundHeading);
-            back(100);
-            turnGunRight(360);
+            ahead(100);
+            turnRadarRight(360);
         }
     }
  
     public void onScannedRobot(ScannedRobotEvent e) {
+    		turnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
+        turnGunRight((getHeading() + e.getBearing()) - getGunHeading());
         fire(5);
-        foundHeading = e.getHeading();
+        
     }
+    
+    public void onHitByBullet(HitByBulletEvent e) {
+    		turnRight(90);
+    }
+    
+    public void onHitWall(HitWallEvent e) {
+    		back(100);
+    		turnLeft(90);
+	}
 }
